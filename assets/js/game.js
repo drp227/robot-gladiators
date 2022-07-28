dbc156cd38a2ae6aa84b75c306fa973c073bee68
drp227 <drp22786@hotmail.com>
2022-07-27 21:59:50 -0500

    fight each robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,8 +6,9 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+            // Alert players that they are starting the round
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // If player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -50,7 +51,10 @@ var fight = function(enemyName) {
 } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+    }
 }
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
